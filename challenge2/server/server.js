4db9978a6a91c02999007d1de0ce6054ae429324
efratcode <efratibri@gmail.com>
2024-12-31 23:55:18 +0200

    part 9
    server side development,
    creating the server and routes files, handaling authentications an a mock database while leaving vulnerabilities for the CTF challenge

--- challenge2/server/server.js
+++ challenge2/server/server.js
@@ -1,8 +1,33 @@
-const express =require('express')
-const app =express()
+//seting up a basic Express application and define the structure for our backend.
 
+// Import necessary modules
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Initialize the Express application
+const app = express();
+const PORT = 5000;
+
+// Middleware to parse incoming JSON requests
+app.use(bodyParser.json());
+
+// Import routes
+const authRoutes = require('./routes/authRoutes');
+const usersRoutes = require('./routes/usersRoutes');
+
+// Use routes in the app
+app.use('/api/auth', authRoutes); // Routes for authentication
+app.use('/api/users', usersRoutes); // Routes for users management
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the CTF backend!');
+  });
+  
 app.get("/api",(req,res) => {
     res.json({"parks": ["Hollywood","Orlando","Japan", "Singapore", "Beijing"] })
 })
 
-app.listen(5000,()=>{console.log("server started on port 5000")})
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
